92dc482c6df55c2113e93bbcf53cc36372d1b2b9
Shirolin <jerrys40506@gmail.com>
2023-06-15 01:50:54 +0900

    統計資料完成

--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -1,7 +1,9 @@
-
 <script>
-import Chart from "chart.js/auto";
+import Chart from 'chart.js/auto';
+
+
 export default {
+
     props: ["isExist", "isActive", "isEnd", "questionnaire"],
     data() {
         return {
@@ -15,46 +17,99 @@ export default {
             reportTime: "",
             reportData: [],
             reporterCount: 0,
+            statisticType: "text",
         }
     },
     methods: {
-        createPieChart() {
-            const pieChartCanvas = this.$refs.pieChartCanvas;
-            const pieChart = new Chart(pieChartCanvas, {
-                type: 'pie',
-                data: {
-                    labels: ['Label 1', 'Label 2', 'Label 3'],
-                    datasets: [{
-                        data: [30, 40, 30],
-                        backgroundColor: ['red', 'blue', 'green'],
-                    }],
-                },
-            });
-        },
-        createBarChart() {
-            const barChartCanvas = this.$refs.barChartCanvas;
-            const barChart = new Chart(barChartCanvas, {
-                type: 'bar',
-                data: {
-                    labels: ['Label 1', 'Label 2', 'Label 3'],
-                    datasets: [{
-                        label: 'Dataset 1',
-                        data: [10, 20, 30],
-                        backgroundColor: 'blue',
-                    }],
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true,
+
+        changeChart() {
+            if (this.statisticType === "text") {
+                this.statisticType = "pie";
+                this.questionList.forEach((question, index) => {
+                    const canvas = document.getElementById('chartPie' + index)
+                    const chartInstance = Chart.getChart(canvas); // 取得原本的圖表實例
+                    if (chartInstance) {
+                        chartInstance.destroy(); // 移除原本的圖表
+                    }
+                    // 創建圖表
+                    new Chart(canvas, {
+                        type: 'pie',
+                        data: {
+                            labels: question.optionList.map(option => option.optionName),
+                            datasets: [
+                                {
+                                    label: 'Data',
+                                    data: question.optionList.map(option => {
+                                        return this.reportData.filter(report => {
+                                            return report.option.optionId === option.optionId
+                                        }).length
+                                    }),
+                                    backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+                                    borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+                                    borderWidth: 1,
+                                    borderWidth: 1,
+                                },
+                            ],
                         },
-                    },
-                },
-            });
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            aspectRatio: 1
+                        }
+                    });
+                })
+                return;
+            }
+            if (this.statisticType === "pie") {
+                this.statisticType = "bar";
+                this.questionList.forEach((question, index) => {
+                    const canvas = document.getElementById('chart' + index)
+                    const chartInstance = Chart.getChart(canvas); // 取得原本的圖表實例
+                    if (chartInstance) {
+                        chartInstance.destroy(); // 移除原本的圖表
+                    }
+                    // 創建圖表
+                    new Chart(canvas, {
+                        type: 'bar',
+                        data: {
+                            labels: question.optionList.map(option => option.optionName),
+                            datasets: [
+                                {
+                                    label: '回答數',
+                                    data: question.optionList.map(option => {
+                                        return this.reportData.filter(report => {
+                                            return report.option.optionId === option.optionId
+                                        }).length
+                                    }),
+                                    backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+                                    borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+                                    borderWidth: 1,
+                                },
+                            ],
+                        },
+                        options: {
+                            indexAxis: 'y', // 設置為 'y' 以生成水平條形圖
+                            scales: {
+                                x: {
+                                    beginAtZero: true
+                                },
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        },
+                    });
+                })
+                return;
+            }
+            if (this.statisticType === "bar") {
+                this.statisticType = "text";
+                return;
+            }
         },
 
     },
-    beforeMount() {
+    created() {
         const sessionQuestionData = sessionStorage.getItem("questionData");
         if (sessionQuestionData) {
             this.questionList = JSON.parse(sessionQuestionData);
@@ -64,7 +119,7 @@ export default {
             this.reportData = JSON.parse(sessionReportData);
         }
     },
-    mounted() {
+    beforeMount() {
         this.questionList = this.questionList.map(question => {
             return fetch("http://localhost:8080/findByQuestions", {
                 method: "POST",
@@ -89,38 +144,21 @@ export default {
         Promise.all(this.questionList).then(updatedQuestions => {
             this.questionList = updatedQuestions;
             console.log(this.questionList);
-            if (sessionStorage.getItem("reportList") && this.readOnly) {
-                const reportList = JSON.parse(sessionStorage.getItem("reportList"));
 
-                console.log(reportList);
-                // 將報告列表放入對應的欄位
-                reportList.forEach(report => {
-                    const questionIndex = this.questionList.findIndex(question => question.questionId === report.question.questionId);
-                    if (questionIndex !== -1) {
-                        const optionIndex = this.questionList[questionIndex].optionList.findIndex(option => option.optionId === report.option.optionId);
-                        if (optionIndex !== -1) {
-                            if (this.questionList[questionIndex].type === 'single') {
-                                this.questionList[questionIndex].selectedOption = report.option.optionId;
-                            } else if (this.questionList[questionIndex].type === 'multiple') {
-                                this.questionList[questionIndex].selectedOption[optionIndex] = true;
-                            }
-                        }
-                    }
-                });
-            }
             if (sessionStorage.getItem("reporterData")) {
                 const reporter = JSON.parse(sessionStorage.getItem("reporterData"))
                 this.reporterCount = reporter.dataCount;
             }
         });
-        this.createPieChart();
-        this.createBarChart();
+
     },
+
+
 
 }
 </script>
 <template>
-    <div class="write">
+    <div class="statistic">
         <div class="questionnaire-area">
             <div class="title">
                 <h2>{{ questionnaire.title }}</h2>
@@ -128,17 +166,18 @@ export default {
             <div class="description">
                 <h4>{{ questionnaire.description }}</h4>
             </div>
+            <button type="button" @click="changeChart">change</button>
             <div class="time">
                 <p>問卷開放時間：{{ questionnaire.startDate }}~{{ questionnaire.endDate }}</p>
             </div>
         </div>
 
         <div class="question-area">
-            <div v-for="(question, index) in questionList" :key="index">
+            <div v-for="(question, index) in questionList" :key="index" class="question">
                 <ul>
                     <li v-if="question.required">{{ index + 1 }}、{{ question.title }} (必填)</li>
                     <li v-else>{{ index + 1 }}、{{ question.title }} (選填)</li>
-                    <p v-for="option in question.optionList" style="margin: 12px;">
+                    <p v-for="option in question.optionList" style="margin: 12px;" v-show="statisticType === 'text'">
                         {{ option.optionName }}：
                         {{ ((reportData.filter(report => {
                             return report.option.optionId === option.optionId
@@ -146,6 +185,10 @@ export default {
                             return report.question.questionId === question.questionId
                         }).length) * 100).toFixed(2) + '%' }}
                     </p>
+                    <canvas :id="'chart' + index" style="width: 128px; max-height: 256px;"
+                        v-show="statisticType === 'bar'"></canvas>
+                    <canvas :id="'chartPie' + index" style="width: 128px; max-height: 256px;"
+                        v-show="statisticType === 'pie'"></canvas>
                 </ul>
             </div>
         </div>
@@ -153,14 +196,14 @@ export default {
             <!-- <button type="button" class="btn btn-secondary" @click="cancel">返回</button> -->
         </div>
     </div>
-    <canvas ref="pieChartCanvas"></canvas>
-    <canvas ref="barChartCanvas"></canvas>
 </template>
 <style lang="scss" scoped>
-.write {
+.statistic {
     margin: 24px;
     border: 1px solid gray;
-    min-width: min-content;
+    max-width: 100vw;
+
+
 
 
     .questionnaire-area {
@@ -182,6 +225,7 @@ export default {
         border: 1px solid gray;
         border-radius: 12px;
         min-width: min-content;
+
     }
 
     .btn-area {
